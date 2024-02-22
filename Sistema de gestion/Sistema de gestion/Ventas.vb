dd75ca8d879d666745ded107f92709df60d65979
FranDesarrolla <franciscovargascontacto@gmail.com>
2024-02-22 16:01:05 -0300

    Cambios en SQL
    Cambios al codigo por modificaciones en la estructura de la base de datos SQL

--- Sistema de gestion/Sistema de gestion/Ventas.vb
+++ Sistema de gestion/Sistema de gestion/Ventas.vb
@@ -12,11 +12,11 @@ Public Class Ventas
             setdedatos.Tables("dtventa").Rows.Clear()
         End If
 
-        Dim consultassql As String = "SELECT NDV.idventa as ID, C.Nombre + ' ' + C.Apellido AS Cliente, E.Nombre + ' ' + E.Apellido AS Empleado, NDV.idcliente, NDV.idempleado, NDV.FechaDeVenta AS Fecha, 
-		                                    NDV.PuntoDeVenta as Sucursal, NDV.NroComprobante as Comprobante, NDV.Letra, NDV.MetodoDePago as Metodo, NDV.TipoFactura as Tipo, NDV.Total, C.Nombre, C.Apellido
+        Dim consultassql As String = "SELECT NDV.IDNotaDeVenta AS ID, C.Nombre + ' ' + C.Apellido AS Cliente, E.Nombre + ' ' + E.Apellido AS Empleado, NDV.Cliente, NDV.Empleado, NDV.FechaDeVenta AS Fecha, 
+		                            NDV.PuntoDeVenta as Sucursal, NDV.NroComprobante as Comprobante, NDV.Letra, NDV.MetodoDePago as Metodo, NDV.TipoFactura as Tipo, NDV.Total, C.Nombre, C.Apellido, C.Direccion
                                     FROM NotasDeVentas NDV
-                                    INNER JOIN Clientes C ON C.idcliente = NDV.idcliente
-                                    INNER JOIN Empleados E ON E.idempleado = NDV.idempleado"
+                                    INNER JOIN Clientes C ON C.Cuenta = NDV.Cliente
+                                    INNER JOIN Empleados E ON E.Cuenta = NDV.Empleado"
 
         Dim adaptadorSql As New SqlDataAdapter(consultassql, conexionSql)
         Dim dtventa As New DataTable
@@ -26,7 +26,7 @@ Public Class Ventas
 
         'CONFIGURAR QUE COLUMNAS SERAN VISIBLES
 
-        Dim columnasOcultar() As Integer = {3, 4, 10, 11, 12, 13}
+        Dim columnasOcultar() As Integer = {3, 4, 10, 11, 12, 13, 14}
 
         For Each columna As Integer In columnasOcultar
             GrillaVentas.Columns(columna).Visible = False
@@ -45,7 +45,7 @@ Public Class Ventas
 
         'COLOCAR QUE SE HAGA .FILL LA GRILLA PARA DELIMITAR EL ESPACIO AL TOTAL DE LA GRILLA
 
-        For i As Integer = 0 To 13
+        For i As Integer = 0 To 14
             GrillaVentas.Columns(i).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
         Next i
 
@@ -59,7 +59,7 @@ Public Class Ventas
         ABM_Ventas.txtCuenta.Text = GrillaVentas.CurrentRow.Cells(3).Value
         ABM_Ventas.lblNombreCliente.Text = GrillaVentas.CurrentRow.Cells(12).Value
         ABM_Ventas.lblApellidoCliente.Text = GrillaVentas.CurrentRow.Cells(13).Value
-        ABM_Ventas.lblApellidoCliente.Text = GrillaVentas.CurrentRow.Cells(14).Value
+        ABM_Ventas.lblDireccion.Text = GrillaVentas.CurrentRow.Cells(14).Value
 
         ModuloPrincipal.AbrirFormEnPanel(ABM_Ventas)
         ModuloSistema.conexionSql.Close()
